b9f967b5b5836b8ad3d6b7ba05afd3cb63c3a6f1
GalaxJ6 <sfernandezga@unal.edu.co>
2023-11-05 23:45:13 -0500

    Se hace el enrutamiento de la página de inicio y de registrarse

--- resources/views/registroDatos.blade.php
+++ resources/views/registroDatos.blade.php
@@ -22,11 +22,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active text-light" aria-current="page" href="{{ route('estructura.blade.php') }}">Iniciar Sesión</a>
+                            <a class="nav-link active text-light" aria-current="page" href="#">Iniciar Sesión</a>
                         </li>
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <a class="nav-link active text-light" aria-current="page" href="#">Registrarse</a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
