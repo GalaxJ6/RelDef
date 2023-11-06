b03409c1c08d28289a97699620eb635cd03e68ae
GalaxJ6 <sfernandezga@unal.edu.co>
2023-11-05 22:15:00 -0500

    Algunas vistas conectadas

---
+++ app/Http/Controllers/RegistroDatos.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistroDatos extends Controller
+{
+    public function registro(){
+        return view('registroDatos'); 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistroDatos;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('estructura');
 });
+
+// Route::get('/estructura', function () {
+//     return view('registroDatos');
+// });
+
+// Route::get('/registroDatos', 'RegistroDatos@registro');
+Route::get('/registroDatos', [RegistroDatos::class, 'registro']);
+// Route::get('/estructura', [RegistroDatos::class, 'registro']);
